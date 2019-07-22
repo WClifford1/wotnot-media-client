088580d492653025b270c4f90e4e32f9d70b6416
fari3001 <fari3001_14@hotmail.com>
2019-07-22 19:20:59 +1000

    Accordion functionality

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import Chevron from './Chevron';
+
 
 export default function Accordion(props) {
+
+    const [setActive, setActiveState] = useState("");
+    const [setHeight, setHeightState] = useState("0px");
+
+    const content = useRef(null)
+
+    function toggleAccordion() {
+        setActiveState(setActive === "" ? "active" : "");
+    }
+
     return (
         <div className="accordion__section">
-            <button className="accordion">
+            <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
                 <p className="accordion__title">{props.title}</p>
+                <Chevron width={10} fill={"#777"} />
             </button>
             <div className="accordion__content">
                 <div className="accordion__text" dangerouslySetInnerHTML={{ __html: props.content }} />
