73c93943ad1501694afc22a9d561a3f932c8205f
fari3001 <fari3001_14@hotmail.com>
2019-07-23 15:16:50 +1000

    Home page images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import heroimg from '../images/blob-shape.svg'
+import blob from '../images/blob-shape.svg'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import HowWeHelp from './HowWeHelp'
 import Service2 from './Service2';
 import WhyUs from './WhyUs'
+import comp from '../images/comp.svg'
 
 
 export default function Home() {
     return (
         <Router>
-        <div className="nav-cont">
-            {/* <div className="nav">
+            <div className="nav-cont">
+                {/* <div className="nav">
                 <h2 className="logo">WotNot Media</h2>
                 <ul className="nav-items">
 
@@ -21,7 +22,7 @@ export default function Home() {
                 </ul>
             </div> */}
 
-            {/* <div className="hero-image">
+                {/* <div className="hero-image">
                 <div className="hero-text">
                     <h1>Lucrative, Innovative, Design Solutions</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga maiores sit voluptates optio odit nihil iusto est sint qui! Exercitationem delectus nihil alias praesentium iste maiores officiis quisquam repellat. Voluptates.</p>
@@ -33,22 +34,29 @@ export default function Home() {
                 <img src={heroimg} className="blob" alt="hoohaa" />
             </div> */}
 
-            <div className="hero-image">
-                <div className="hero-text">
-                    <h1>Lucrative, Innovative, Design Solutions</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga maiores sit voluptates optio odit nihil iusto est sint qui! Exercitationem delectus nihil alias praesentium iste maiores officiis quisquam repellat. Voluptates.</p>
-                    <div className="hbtn-cont">
-                        <button type="button" class="hero-btn">Enquire Today!</button>
+                <div className="hero-image">
+                    <div className="hero-text">
+                        <h1>Lucrative, Innovative, Design Solutions</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga maiores sit voluptates optio odit nihil iusto est sint qui! Exercitationem delectus nihil alias praesentium iste maiores officiis quisquam repellat. Voluptates.</p>
+                        <div className="hbtn-cont">
+                            <button type="button" class="hero-btn">Enquire Today!</button>
+                        </div>
+                    </div>
+                    <div className="image-home">
+                        <div className="blob">
+                            <img src={blob} alt="hoohaa" />
+                        </div>
+                        <div className="comp-img" >
+                            <img src={comp} alt="hoohaa" />
+                        </div>
                     </div>
                 </div>
-                <img src={heroimg} className="blob" alt="hoohaa" />
+
+                <Service2 />
+                <HowWeHelp />
+                <WhyUs />
+
             </div>
-
-            <Service2 />
-            <HowWeHelp />
-            <WhyUs />
-
-        </div>
         </Router>
     )
 }
