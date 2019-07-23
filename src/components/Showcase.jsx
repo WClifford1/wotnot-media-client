59db8edb9bae9f9367f754c3fdcebfff8b78ab91
fari3001 <fari3001_14@hotmail.com>
2019-07-23 19:34:29 +1000

    More grid

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,6 +3,10 @@ import tak from '../images/grid6.png'
 import tak2 from '../images/grid14.png'
 import tak3 from '../images/grid16.png'
 import tak4 from '../images/grid18.png'
+import tak5 from '../images/grid20.png'
+import tak6 from '../images/grid22.png'
+import tak7 from '../images/grid24.png'
+import tak8 from '../images/grid26.png'
 
 export default function Showcase() {
     return (
@@ -15,10 +19,10 @@ export default function Showcase() {
                 <div className="grid-item"><img src={tak3} alt="hoohaa" /></div>
                 <div className="grid-item"><img src={tak4} alt="hoohaa" /></div>
                 <div className="grid-item"><img src={tak} alt="hoohaa" /></div>
-                <div className="grid-item"><img src={tak} alt="hoohaa" /></div>
-                <div className="grid-item"><img src={tak2} alt="hoohaa" /></div>
-                <div className="grid-item"><img src={tak} alt="hoohaa" /></div>
-                <div className="grid-item"><img src={tak2} alt="hoohaa" /></div>
+                <div className="grid-item"><img src={tak5} alt="hoohaa" /></div>
+                <div className="grid-item"><img src={tak6} alt="hoohaa" /></div>
+                <div className="grid-item"><img src={tak7} alt="hoohaa" /></div>
+                <div className="grid-item"><img src={tak8} alt="hoohaa" /></div>
             </div>
         </React.Fragment>
     )
