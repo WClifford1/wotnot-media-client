d60fc98fcbdd8c49de7cda29b39d794ad42538f9
fari3001 <fari3001_14@hotmail.com>
2019-07-24 17:54:04 +1000

    Service routes added

--- src/components/ServicePage.jsx
+++ src/components/ServicePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function TestCard() {
     return (
@@ -19,7 +20,7 @@ export default function TestCard() {
            </a>
           <div class="card__info">
             <span class="card__category">Print</span>
-            <h3 class="card__title">Professional Printing Services</h3>
+            <h3 class="card__title"><a href="#"><Link to="/print">Professional Printing Services</Link></a></h3>
             <span class="card__by">by <a href="#" class="card__author" title="author">Wotnot Media</a></span>
           </div>
         </article>
@@ -35,7 +36,7 @@ export default function TestCard() {
            </a>
           <div class="card__info">
             <span class="card__category">Web Design</span>
-            <h3 class="card__title">Innovative Web Solutions</h3>
+            <h3 class="card__title"><a href="#"><Link to="/web">Innovative Web Solutions</Link></a></h3>
             <span class="card__by">by <a href="#" class="card__author" title="author">Wotnot Media</a></span>
           </div>
         </article>  
@@ -50,13 +51,10 @@ export default function TestCard() {
            </a>
           <div class="card__info">
             <span class="card__category">Video Content</span>
-            <h3 class="card__title">Save Your Precious Moments</h3>
+            <h3 class="card__title"><a href="#"><Link to="/video">Save Your Precious Moments</Link></a></h3>
             <span class="card__by">by <a href="#" class="card__author" title="author">Wotnot Media</a></span>
           </div>
-        </article>  
-          
-          
-          
+        </article>        
           </section>
           </div>
           </div>
